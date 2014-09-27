9975eee495afd2ad9d94330c2b1ed87ce5f3bd1f
BillBai <BillBai42@gmail.com>
2014-09-27 21:43:55 +0800

    UI improvement

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,5 @@
 class Photo < ActiveRecord::Base
-  belongs_to :content
+  belongs_to :content, dependent: :destroy
 
   has_attached_file :image, :styles => { :medium => "800x500>", :thumb => "160x90>" }
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
