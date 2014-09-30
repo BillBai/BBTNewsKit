81a3bd9de54b87c8f3f01716ada43a5baacc6742
BillBai <BillBai42@gmail.com>
2014-09-30 22:45:09 +0800

    small UI tewak

--- app/models/section.rb
+++ app/models/section.rb
@@ -4,5 +4,5 @@ class Section < ActiveRecord::Base
   enum category: [:news, :special, :weekly, :video]
 
   has_attached_file :section_image, :styles => { :medium => "300x300>"}, :default_url => "/images/:style/missing.png"
-  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  validates_attachment_content_type :section_image, :content_type => /\Aimage\/.*\Z/
 end
