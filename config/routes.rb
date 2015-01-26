fc146d3c3c2a855121142db9cf81e1272cd8bdec
aczero <282493754@qq.com>
2015-01-26 21:16:53 +0800

    init devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, controllers: { sessions: "users/sessions" ,registrations: "users/registrations"}
   root 'contents#index'
 
   constraints subdomain: 'api' do
