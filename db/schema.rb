83553b0ab1abf24c8f05c5ca20bcfe07bca3afde
aczero <282493754@qq.com>
2015-01-28 15:02:11 +0800

    fix user_group bug

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150128062016) do
+ActiveRecord::Schema.define(version: 20150128065626) do
 
   create_table "article_body_images", force: true do |t|
     t.datetime "created_at"
@@ -101,8 +101,8 @@ ActiveRecord::Schema.define(version: 20150128062016) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
-    t.integer  "group"
     t.string   "display_name"
+    t.integer  "group",                  default: 0
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
