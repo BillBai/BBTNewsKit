ae9465ae8bfb5cda23dfc5a295f488409ebb2247
aczero <282493754@qq.com>
2015-03-13 15:27:59 +0800

    modified image size

--- app/models/article_body_image.rb
+++ app/models/article_body_image.rb
@@ -1,6 +1,6 @@
 class ArticleBodyImage < ActiveRecord::Base
   belongs_to :content
   #has_attached_file :body_image
-  has_attached_file :body_image, :styles => { :medium => "80%"}
+  has_attached_file :body_image, :styles => {:original => "1280x720" :medium => "50%"}
   validates_attachment_content_type :body_image, :content_type => /\Aimage\/.*\Z/
 end
